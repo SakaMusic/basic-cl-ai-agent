4a682094a1c3af2236f7b9460751e633b781e4f2
SakaMusic <wshbenson@gmail.com>
2026-02-24 00:35:56 -0700

    Added all calculator app files

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 
 def main():
-    print("Hello from ai-agent!")
     load_dotenv()
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("api key has not been found in .env")
     
+    # Get user prompt
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    # Store user prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
+    # Instantiate AI agent
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=args.user_prompt
+        model="gemini-2.5-flash", contents=messages
     )
+
     if response.usage_metadata != None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if args.verbose == True:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
         raise RuntimeError("Failed API request, no response metadata")
+    
     print(response.text)
 
 if __name__ == "__main__":
